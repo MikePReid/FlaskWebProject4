6ed2194c8d15782be355b59332e648dbab297b08
Mike Reid <mikerinar@gmal.com>
2020-04-04 20:28:56 -0400

    Add Flask Web project

--- FlaskWebProject4/runserver.py
+++ FlaskWebProject4/runserver.py
@@ -1,6 +1,8 @@
 import os
 from HelloFlask import app    # Imports the code from HelloFlask/__init__.py
 
+# ref: https://docs.microsoft.com/en-us/visualstudio/python/learn-flask-visual-studio-step-01-project-solution?view=vs-2019
+
 if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
 
