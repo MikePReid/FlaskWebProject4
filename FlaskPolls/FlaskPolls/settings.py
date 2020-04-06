bbe6b1bd2a8390a76e9586409411fc3a0b29c0f0
Mike Reid <mikerinar@gmal.com>
2020-04-05 20:52:48 -0400

    End of tutorial

--- FlaskPolls/FlaskPolls/settings.py
+++ FlaskPolls/FlaskPolls/settings.py
@@ -8,6 +8,14 @@ environment variables, or set the default values in code here.
 from os import environ
 
 REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'memory')
+#If you want to use Azure table store or Mondo DB, you must first initialize those data stores elsewhere,
+#then set the necessary environment variables that tell the app how to connect to the store.
+
+#The implementations of the Repository class that are specific to each data store can be found in 
+#models\azuretablestorage.py, models\mongodb.py, and models\memory.py. The Azure storage implementation uses the 
+#azure-storage package; the Mongo DB implementation uses the pymongo package. Both packages are included in the 
+#project template's requirements.txt file.
+
 
 if REPOSITORY_NAME == 'azuretablestorage':
     REPOSITORY_SETTINGS = {
